272275e7126f22d22ac87874fc0829a5e97a88b7
nagendra <nagendraputhane@gmail.com>
2019-08-11 23:42:26 +0530

    small tweaks

--- strawberry_opencv.py
+++ strawberry_opencv.py
@@ -13,22 +13,30 @@ def show(image):
     plt.imshow(image, interpolation = 'nearest')
 
 def overlay_mask(mask, image):
+    #make the mask rgb
     rgb_mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+    #calculate the weighted sum of two arrays
     img = cv2.addWeighted(rgb_mask, 0.5, image, 0.5, 0)
     return img
 
 def find_biggest_contour(image):
+    #copy of the image as findContour function modifies the source image
     image = image.copy()
+    # Contours can be explained simply as a curve joining all the continous points having the same color or intensity
     contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    #isolate largest contour (area)
     contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
     biggest_contour = max(contour_sizes, key = lambda x: x[0])[1]
+    #draw it filled in
     mask  = np.zeros(image.shape, np.uint8)
     cv2.drawContours(mask, [biggest_contour], -1, 255, -1)
     return biggest_contour, mask
 
 def circle_contour(image, contour):
+    #bounding elllipse
     image_with_ellipse = image.copy()
     ellipse = cv2.fitEllipse(contour)
+    #add it
     cv2.ellipse(image_with_ellipse, ellipse, green, 2, cv2.LINE_AA)
     return image_with_ellipse
 
@@ -50,13 +58,12 @@ def find_strawberry(image):
     # Convert to HSV scheme
     image_blur_hsv = cv2.cvtColor(image_blur, cv2.COLOR_RGB2HSV)
 
-    # Filter by color
+    # 0-10 hue, 100+ saturation, 80+ value
     min_red = np.array([0, 100, 80])
     max_red = np.array([10, 256, 256])
-    #layer
     mask1 = cv2.inRange(image_blur_hsv, min_red, max_red)
 
-    # Filter by brightness
+    # 170-180 hue, 100+ saturation, 80+ value
     min_red2 = np.array([170, 100, 80])
     max_red2 = np.array([180, 256, 256])
     #layer
@@ -65,8 +72,10 @@ def find_strawberry(image):
     # Combine masks
     mask = mask1 + mask2
 
-    # Clean up
+    # Elliptical kernel
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+    #opening: erosion followed by dilation - removing noise
+    #closing: dilation followed by erosion - closing small holes
     mask_closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     mask_clean = cv2.morphologyEx(mask_closed, cv2.MORPH_OPEN, kernel)
 
